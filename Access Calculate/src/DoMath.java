2fc657f65f33d7559ef661345b8808c8eb7e6630
Marlon Malva <me8fish@gmail.com>
2019-09-02 23:54:23 -0700

    Completed part 2 of Calculator Project

--- Access Calculate/src/DoMath.java
+++ Access Calculate/src/DoMath.java
@@ -7,12 +7,21 @@ public class DoMath
     	  Calculate.cube(4);
     	  System.out.println(Calculate.average(2 , 3 ));
           System.out.println(Calculate.average(2 , 4 ,6 ));
-          System.out.println(Calculate.toDegees(1.6));
+          System.out.println(Calculate.toDegrees(1.6));
           System.out.println(Calculate.toRadians(180));
-          System.out.println(Calculate.discriminat(2 ,4 ,8 ));
+          System.out.println(Calculate.discriminant(2 ,4 ,8 ));
           System.out.println(Calculate.toImproperFrac(3,1,2));
           System.out.println(Calculate.toMixedNum(7,2));
           System.out.println(Calculate.foil(2,3,6,-7,"n"));
+          
+          // new stuff 9/1
+          System.out.println(Calculate.isDivisibleBy(8,4));
+          System.out.println(Calculate.absValue(-400));
+          System.out.println(Calculate.absValue(270));
+          System.out.println(Calculate.max (5,7));
+          System.out.println(Calculate.max(4,6,9));
+          System.out.println(Calculate.min(5,14));
+          System.out.println(Calculate.round2(1.47683));
 
        //   System.out.println(Calculate.);
       }
--- Calculate Library/src/Calculate.java
+++ Calculate Library/src/Calculate.java
@@ -1,6 +1,6 @@
-
 public class Calculate {
-    public static int square(int num) {
+
+	public static int square(int num) {
         int squaredNum = num * num;
         return squaredNum;
 
@@ -23,7 +23,7 @@ public class Calculate {
     }
 
 
-    public static double toDegees(double angle) {
+    public static double toDegrees(double angle) {
         double deg = 180 * angle / 3.14159;
         return deg;
     }
@@ -34,7 +34,7 @@ public class Calculate {
         return rad;
     }
 
-    public static double discriminat(double a, double b, double c) {
+    public static double discriminant(double a, double b, double c) {
         double ret = b * b / (4 * a * c);
         return ret;
     }
@@ -64,12 +64,87 @@ public class Calculate {
         String last = h + var+ " ^2" + "+ " + f1+" " + g1;
         return last;
 
-
-
     }
+    
+    public static boolean isDivisibleBy (int a, int b ) {
+	    boolean isDivisibleBy1 =  (a %  b) == 0 ;
+	    return isDivisibleBy1;
+    }
+    
+   public static double absValue (double num) {
+	   if ( num < 0) return (-1) * num;
+	   else return num;
+   }
+   
+   
+    public static double max(double a , double b ) {
+      if (a > b) 
+      { 
+           return a;
+       } 
+       else 
+       {
+             return b;
+       } 
+    }
+    
+ 
+    public static double max(double a, double b, double c) {
+    	// return max(max(a,b), c);{
+    	
+    	double max;
+    	
+    	if (a>=b) {
+    		if (a>=c) {
+    			max=a;
+    		} else {
+    			max=c;
+    		}
+    	} else { // b > a
+			if (b>=c) {
+				max = b;
+			} else {
+				max = c;
+			}
+    	}
+    	
+    	return max;
+    }
+    
+    public static double min(double a , double b ) {
+        if (a < b) 
+        { 
+             return a;
+         } 
+         else 
+         {
+               return b;
+         } 
+    }
+    
+    
+    public static double round2(double num) {
+	    long factor = 100L;
+	    double value = num * factor;
+	    long tmp = Math.round(value);
+	    return (double) tmp / factor;    	
+    }
+
+//    public static double round2b(double num) {
+//    	// 1.34687 -> 1346.87
+//	    int factor = 1000;
+//	    int value = (int) num * factor; // 1346
+//	    
+//	    // here: 6
+//	    int lastDigit = value % 10;
+//	    if (lastDigit >=5) {
+//	    	// round up: desired: 1.35 which is 1350 / 1000.0
+//	    	return (double) value / 1000.0;
+//	    	
+//	    } else {
+//	    	// round down
+//	    	return (double) value / 1000.0;
+//	    }  	
+//    }
+    
 }
-
-
-
-
-
