eb302343a8307ff07a6d5810d0c5e05ce6d6e6f1
Marlon Malva <me8fish@gmail.com>
2019-09-10 07:43:36 -0700

    Format Code (select all code (CTRL-A) & click Eclipse menu: Source | Format)

--- Access Calculate/src/DoMath.java
+++ Access Calculate/src/DoMath.java
@@ -1,39 +1,41 @@
 
 public class DoMath 
-{ 
-	public static void main(String[]args) {    	  
-          System.out.println("\nPart 1");
-		  System.out.println(Calculate.square(7));
-		  System.out.println(Calculate.cube(4));
-    	  System.out.println(Calculate.average(2 , 3 ));
-          System.out.println(Calculate.average(2 , 4 ,6 ));
-          System.out.println(Calculate.toDegrees(1.6));
-          System.out.println(Calculate.toRadians(180));
-          System.out.println(Calculate.discriminant(2 ,4 ,8 ));
-          System.out.println(Calculate.discriminant(0.0, 0.0, 0.0));
-          System.out.println(Calculate.toImproperFrac(3,1,2));
-          System.out.println(Calculate.toMixedNum(7,2));
-          System.out.println(Calculate.foil(2,3,6,-7,"n"));
-          
-          // new stuff 9/1
-          System.out.println("\nPart 2");
-          System.out.println(Calculate.isDivisibleBy(8,4));
-          System.out.println(Calculate.absValue(-400));
-          System.out.println(Calculate.absValue(270));
-          System.out.println(Calculate.max (5,7));
-          System.out.println(Calculate.max(4,6,9));
-          System.out.println(Calculate.min(5,14));
-          System.out.println(Calculate.round2 (1.47383));
-          System.out.println(Calculate.round2 (1.34687));
-          
-          // part 3 (9/8)
-          System.out.println("\nPart 3");
-          System.out.println(Calculate.exponent(2,4));
-          System.out.println(Calculate.factorial(5));
-          System.out.println(Calculate.isPrime(59));
-          System.out.println(Calculate.isPrime(51));
-          System.out.println(Calculate.gcf(25, 15));
-          System.out.println(Calculate.gcf(15, 25));
-          System.out.println(Calculate.gcf(17, 21));
-      }
+{
+	public static void main(String[]args) {
+		
+		System.out.println("\nPart 1");
+		System.out.println(Calculate.square(7));
+		System.out.println(Calculate.cube(4));
+		System.out.println(Calculate.average(2, 3));
+		System.out.println(Calculate.average(2, 4, 6));
+		System.out.println(Calculate.toDegrees(1.6));
+		System.out.println(Calculate.toRadians(180));
+		System.out.println(Calculate.discriminant(2, 4, 8));
+		System.out.println(Calculate.discriminant(0.0, 0.0, 0.0));
+		System.out.println(Calculate.toImproperFrac(3, 1, 2));
+		System.out.println(Calculate.toMixedNum(7, 2));
+		System.out.println(Calculate.foil(2, 3, 6, -7, "n"));
+
+		// new stuff 9/1
+		System.out.println("\nPart 2");
+		System.out.println(Calculate.isDivisibleBy(8, 4));
+		System.out.println(Calculate.absValue(-400));
+		System.out.println(Calculate.absValue(270));
+		System.out.println(Calculate.max(5, 7));
+		System.out.println(Calculate.max(4, 6, 9));
+		System.out.println(Calculate.min(5, 14));
+		System.out.println(Calculate.round2(1.47383));
+		System.out.println(Calculate.round2(1.34687));
+
+		// part 3 (9/8)
+		System.out.println("\nPart 3");
+		System.out.println(Calculate.exponent(2, 4));
+		System.out.println(Calculate.factorial(5));
+		System.out.println(Calculate.isPrime(59));
+		System.out.println(Calculate.isPrime(51));
+		System.out.println(Calculate.gcf(25, 15));
+		System.out.println(Calculate.gcf(15, 25));
+		System.out.println(Calculate.gcf(17, 21));
+		
+	}
 }
--- Calculate Library/src/Calculate.java
+++ Calculate Library/src/Calculate.java
@@ -1,128 +1,119 @@
 public class Calculate {
 
 	public static int square(int num) {
-        int squaredNum = num * num;
-        return squaredNum;
+		int squaredNum = num * num;
+		return squaredNum;
 
-    }
+	}
 
-    public static int cube(int num) {
-        int cubeNum = num * num * num;
-        return cubeNum;
+	public static int cube(int num) {
+		int cubeNum = num * num * num;
+		return cubeNum;
 
-    }
+	}
 
-    public static double average(double num1, double num2) {
-        double average = ((num1 + num2) / 2);
-        return average;
-    }
+	public static double average(double num1, double num2) {
+		double average = ((num1 + num2) / 2);
+		return average;
+	}
 
-    public static double average(double num1, double num2, double num3) {
-        double ret = ((num1 + num2 + num3) / 3);
-        return ret;
-    }
+	public static double average(double num1, double num2, double num3) {
+		double ret = ((num1 + num2 + num3) / 3);
+		return ret;
+	}
 
+	public static double toDegrees(double angle) {
+		double deg = 180 * angle / 3.14159;
+		return deg;
+	}
 
-    public static double toDegrees(double angle) {
-        double deg = 180 * angle / 3.14159;
-        return deg;
-    }
+	public static double toRadians(double angle) {
+		double rad = 3.14159 * angle / 180;
+		return rad;
+	}
 
+	public static double discriminant(double a, double b, double c) {
+		double ret = b * b - (4 * a * c);
+		return ret;
+	}
 
-    public static double toRadians(double angle) {
-        double rad = 3.14159 * angle / 180;
-        return rad;
-    }
+	public static String toImproperFrac(int whole, int numerator, int denominator) {
+		int first = whole * denominator + numerator;
+		String s = first + "/" + denominator;
+		return s;
+	}
 
-    public static double discriminant(double a, double b, double c) {
-        double ret = b * b - (4 * a * c);
-        return ret;
-    }
+	public static String toMixedNum(int numerator, int denominator) {
+		int first = numerator / denominator;
+		int m = first * denominator;
+		int f = numerator - m;
+		String s = first + "_" + f + "/" + denominator;
+		return s;
 
-    public static String toImproperFrac(int whole, int numerator, int denominator) {
-        int first = whole * denominator + numerator;
-        String s = first + "/" + denominator;
-        return s;
-    }
+	}
 
-    public static String toMixedNum(int numerator, int denominator) {
-        int first = numerator / denominator;
-        int m = first * denominator;
-        int f = numerator - m;
-        String s =  first + "_" + f + "/" + denominator;
-        return  s;
+	public static String foil(int ax, int b, int cx, int d, String var) {
+		int step1 = (ax + b) * (cx + d);
+		int h = ax * cx;
+		int h1 = ax * d;
+		int g = b * cx;
+		int g1 = b * d;
+		int f1 = h1 + g;
+		String last = h + var + " ^2" + "+ " + f1 + " " + g1;
+		return last;
 
-    }
+	}
 
-    public static String foil (int ax, int b, int cx, int d, String var) {
-        int step1 = (ax + b) * (cx + d);
-        int  h= ax*cx;
-        int h1= ax*d;
-        int g= b*cx;
-        int g1= b*d;
-        int  f1=h1+g;
-        String last = h + var+ " ^2" + "+ " + f1+" " + g1;
-        return last;
+	public static boolean isDivisibleBy(int a, int b) {
+		boolean isDivisibleBy1 = (a % b) == 0;
+		return isDivisibleBy1;
+	}
 
-    }
-    
-    public static boolean isDivisibleBy (int a, int b ) {
-	    boolean isDivisibleBy1 =  (a %  b) == 0 ;
-	    return isDivisibleBy1;
-    }
-    
-   public static double absValue (double num) {
-	   if ( num < 0) return (-1) * num;
-	   else return num;
-   }
-   
-   
-    public static double max(double a , double b ) {
-      if (a > b) 
-      { 
-           return a;
-       } 
-       else 
-       {
-             return b;
-       } 
-    }
-    
- 
-    public static double max(double a, double b, double c) {
-    	// return max(max(a,b), c);{
-    	
-    	double max;
-    	
-    	if (a>=b) {
-    		if (a>=c) {
-    			max=a;
-    		} else {
-    			max=c;
-    		}
-    	} else { // b > a
-			if (b>=c) {
+	public static double absValue(double num) {
+		if (num < 0)
+			return (-1) * num;
+		else
+			return num;
+	}
+
+	public static double max(double a, double b) {
+		if (a > b) {
+			return a;
+		} else {
+			return b;
+		}
+	}
+
+	public static double max(double a, double b, double c) {
+		// return max(max(a,b), c);{
+
+		double max;
+
+		if (a >= b) {
+			if (a >= c) {
+				max = a;
+			} else {
+				max = c;
+			}
+		} else { // b > a
+			if (b >= c) {
 				max = b;
 			} else {
 				max = c;
 			}
-    	}
-    	
-    	return max;
-    }
-    
-    public static int min(int a , int b ) {
-        if (a < b) 
-        { 
-             return a;
-         } 
-         else 
-         {
-               return b;
-         } 
-    }
-    
-    
+		}
+
+		return max;
+	}
+
+	public static int min(int a, int b) {
+		if (a < b) {
+			return a;
+		} else {
+			return b;
+		}
+	}
+
 //    rounding a double to two decimal points w/ Math library
 //    public static double round2(double num) {
 //	    long factor = 100L;
@@ -131,87 +122,97 @@ public class Calculate {
 //	    return (double) tmp / factor;    	
 //    }
 
+	// rounding a double to two decimal points w/o use of library
+	// anything with +n.nn5 and greater will be rounded up
+	// anything with -n.nn5 will be rounded down
+	public static double round2(double num) {
+		if (num < 0.0)
+			return (-1.0) * round2(-num); // for negative numbers we invert them
 
-    // rounding a double to two decimal points w/o use of library
-    // anything with +n.nn5 and greater will be rounded up
-    // anything with -n.nn5 will be rounded down
-    public static double round2(double num) {    	
-    	if (num < 0.0) return (-1.0) * round2(-num); // for negative numbers we invert them
+		// convert to integer with last digit being the one deciding the rounding, e.g.
+		// 1.34687 -> 1346
+		int factor = 1000;
+		int value = (int) (num * factor); // 1346
 
-    	// convert to integer with last digit being the one deciding the rounding, e.g. 1.34687 -> 1346
-	    int factor = 1000;
-	    int value = (int) (num * factor); // 1346
-	    
-	    // get the last digit which decides if round up or down, here: 6
-	    int lastDigit = value % 10;
-	    if (lastDigit >=5) {
-	    	// round up: desired: 1.35 which is 1350 / 1000.0
-	    	value += (10 - lastDigit); // 1346 + (10 - 6) = 1350	    	
-	    } else {
-	    	// round down
-	    	value -= lastDigit; // 1346 --> 1340
-	    }  	
-    	value = value / 10; // remove the trailing zero, e.g. 1350 --> 135
-    	double ret = value; // convert to double, e.g. 135.0
-    	return ret / 100.0D; // 1.35
-    }
-    
-    // simplified version which leverages that (int) of num * 100 is already the round-down result
-    // rounding a double to two decimal points w/o use of library
-    // anything with +n.nn5 and greater will be rounded up
-    // anything with -n.nn5 will be rounded down
-    public static double round2c(double num) {    	
-    	if (num < 0.0) return (-1.0) * round2c(-num);     // for negative numbers we invert them
-    	int number = (int) (num * 100);                   // these are the digits we want to keep
-    	int significantDigit = (int) (num * 1000) % 10;   // this digit decides the rounding
-    	if (significantDigit >= 5) ++number;              // rounding up if next digit (1000th) is >= 5
-    	return (double) number / 100.0D;
-    }
-    
-    public static double exponent(double base, int exponentx) {
-    	double x = 1;
-    	int a = exponentx ;
-    	int b = exponentx * exponentx;
-    	for (int i = 1 ; i <= exponentx ; ++i) {
-    	     x = x *base;
-    	}
-    	return x;
-    }
-    
-    public static int factorial(int f) {
-    	int x = 1;
-    	for ( int i = 1 ; i <=f ; ++i) {
-    		x = x * i ;
-    	}
-    	return x;
-    	
-    }
-    public static boolean isPrime(int a) {
-    	for (int i = 2 ;i  < a ; ++i) {
-         if (isDivisibleBy (a,i) ) {
-        	 return false;
-         }
-    	}
-        return true;    	
-    }
-    
-     public static int gcf( int a , int b ) {
-    	 int i = min(a,b) ;
-    	 while (i > 1 ) {
-    		 if ( isDivisibleBy(a,i) && isDivisibleBy(b,i) ) return i;
-    		 --i;
-    	 }
-    	 return 1;
-     }
-    
-    // A call to sqrt returns an approximation of the square root of the value passed, rounded to two decimal places.  
-    // The method accepts a double and returns a double.
-    // While the classic algorithm uses recursion (a topic we will all explore later) for this first pass, 
-    // you should Newton's method for approximating square roots.  
-    // Essentially, it employs some fancy guess and check to find an appropriate value.  
-    // The algorithm you are to use is explained pretty clearly here: goo.gl/Vl3JGe
-    //    You should keep guessing and altering your guesses until the difference between 
-    //    the original input and your result squared is less than .005.  
-    //    Then round to 2 decimal places.
-  //  sqrt
+		// get the last digit which decides if round up or down, here: 6
+		int lastDigit = value % 10;
+		if (lastDigit >= 5) {
+			// round up: desired: 1.35 which is 1350 / 1000.0
+			value += (10 - lastDigit); // 1346 + (10 - 6) = 1350
+		} else {
+			// round down
+			value -= lastDigit; // 1346 --> 1340
+		}
+		value = value / 10; // remove the trailing zero, e.g. 1350 --> 135
+		double ret = value; // convert to double, e.g. 135.0
+		return ret / 100.0D; // 1.35
+	}
+
+	// simplified version which leverages that (int) of num * 100 is already the
+	// round-down result
+	// rounding a double to two decimal points w/o use of library
+	// anything with +n.nn5 and greater will be rounded up
+	// anything with -n.nn5 will be rounded down
+	public static double round2c(double num) {
+		if (num < 0.0)
+			return (-1.0) * round2c(-num); // for negative numbers we invert them
+		int number = (int) (num * 100); // these are the digits we want to keep
+		int significantDigit = (int) (num * 1000) % 10; // this digit decides the rounding
+		if (significantDigit >= 5)
+			++number; // rounding up if next digit (1000th) is >= 5
+		return (double) number / 100.0D;
+	}
+
+	public static double exponent(double base, int exponentx) {
+		double x = 1;
+		int a = exponentx;
+		int b = exponentx * exponentx;
+		for (int i = 1; i <= exponentx; ++i) {
+			x = x * base;
+		}
+		return x;
+	}
+
+	public static int factorial(int f) {
+		int x = 1;
+		for (int i = 1; i <= f; ++i) {
+			x = x * i;
+		}
+		return x;
+
+	}
+
+	public static boolean isPrime(int a) {
+		for (int i = 2; i < a; ++i) {
+			if (isDivisibleBy(a, i)) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public static int gcf(int a, int b) {
+		int i = min(a, b);
+		while (i > 1) {
+			if (isDivisibleBy(a, i) && isDivisibleBy(b, i))
+				return i;
+			--i;
+		}
+		return 1;
+	}
+
+	// A call to sqrt returns an approximation of the square root of the value
+	// passed, rounded to two decimal places.
+	// The method accepts a double and returns a double.
+	// While the classic algorithm uses recursion (a topic we will all explore
+	// later) for this first pass,
+	// you should Newton's method for approximating square roots.
+	// Essentially, it employs some fancy guess and check to find an appropriate
+	// value.
+	// The algorithm you are to use is explained pretty clearly here: goo.gl/Vl3JGe
+	// You should keep guessing and altering your guesses until the difference
+	// between
+	// the original input and your result squared is less than .005.
+	// Then round to 2 decimal places.
+	// sqrt
 }
