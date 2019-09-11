e704c63b0b175666598fc4bea8a6310e5029d126
Marlon Malva <me8fish@gmail.com>
2019-09-10 21:17:45 -0700

    Fixed boundaries to have error less than 0.005

--- Calculate Library/src/Calculate.java
+++ Calculate Library/src/Calculate.java
@@ -207,9 +207,9 @@ public class Calculate {
 		double a = 2;
 	    double b  = 0.5*((num/a)+a);
 	    double x = (b*b);
-	    if (x > num);
+	    
 	    double error = (x-num);
-	    if (( error <= 0.005)&&(error >=-0.005))
+	    if (( error < 0.005)&&(error > -0.005))
 	    	return b;
 	    
         while ((error >=0.005)||(error <=-0.005)) {
@@ -219,7 +219,23 @@ public class Calculate {
         	error = (x-num);
         }
         return b ; 
-	}	
-	
-}
+	}
+	    
+	}
+//	√ N ≈ ½(N/A + A)
 
+
+
+	// A call to sqrt returns an approximation of the square root of the value
+	// passed, rounded to two decimal places.
+	// The method accepts a double and returns a double.
+	// While the classic algorithm uses recursion (a topic we will all explore
+	// later) for this first pass,
+	// you should Newton's method for approximating square roots.
+	// Essentially, it employs some fancy guess and check to find an appropriate
+	// value.
+	// The algorithm you are to use is explained pretty clearly here: goo.gl/Vl3JGe
+	// You should keep guessing and altering your guesses until the difference
+	// between
+	// the original input and your result squared is less than .005.
+	// Then round to 2 decimal places.
